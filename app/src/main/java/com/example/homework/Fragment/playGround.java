4021d4ac06a4f0d2220f16f22b2575658553802b
Gentle <115886697+Gentlemuqiu@users.noreply.github.com>
2023-01-25 23:18:06 +0800

    实现体系页面

--- app/src/main/java/com/example/homework/Fragment/playGround.java
+++ app/src/main/java/com/example/homework/Fragment/playGround.java
@@ -1,27 +1,38 @@
 package com.example.homework.Fragment;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.viewpager.widget.ViewPager;
 
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.TextView;
-
+import com.example.homework.Adapter.playGroundChildAdapter;
 import com.example.homework.R;
+import com.example.homework.childFragment.Navigation;
+import com.example.homework.childFragment.account;
+import com.example.homework.childFragment.item;
+import com.example.homework.childFragment.search;
+import com.example.homework.childFragment.system;
+import com.google.android.material.tabs.TabLayout;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class playGround extends Fragment {
 
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
-
     private String mParam1;
     private String mParam2;
-
-    private TextView mTextView;
+    private ViewPager mViewPager;
+    private TabLayout mTabLayout;
+    private List<Fragment> mFragments;
+    private List<String> mTitleName;
+    private playGroundChildAdapter mPlayGroundChildAdapter;
     public playGround() {
     }
 
@@ -52,7 +63,31 @@ public class playGround extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        mTextView=view.findViewById(R.id.tv_playGround);
-        mTextView.setText(mParam1);
+        mViewPager=view.findViewById(R.id.vp_playground);
+        mTabLayout=view.findViewById(R.id.tab_layout);
+        initData();
+        mPlayGroundChildAdapter=new playGroundChildAdapter(getActivity().getSupportFragmentManager(), mFragments,mTitleName);
+        mViewPager.setAdapter(mPlayGroundChildAdapter);
+        mTabLayout.setupWithViewPager(mViewPager);
+    }
+
+    private void initData() {
+        mFragments=new ArrayList<>();
+        system system= com.example.homework.childFragment.system.newInstance("体系","");
+        Navigation navigation=com.example.homework.childFragment.Navigation.newInstance("导航","");
+        item item=com.example.homework.childFragment.item.newInstance("项目","");
+        search search=com.example.homework.childFragment.search.newInstance("发现","");
+        account account=com.example.homework.childFragment.account.newInstance("导航","");
+        mFragments.add(system);
+        mFragments.add(navigation);
+        mFragments.add(item);
+        mFragments.add(search);
+        mFragments.add(account);
+        mTitleName=new ArrayList<>();
+        mTitleName.add("体系");
+        mTitleName.add("导航");
+        mTitleName.add("项目");
+        mTitleName.add("搜索");
+        mTitleName.add("公众号");
     }
 }
