ce9fd427710a7670ae53431aece88697ec2af38a
Gentle <115886697+Gentlemuqiu@users.noreply.github.com>
2023-01-26 16:49:11 +0800

    利用两个Recycler实现导航, 利用spinner及Recycler实现项目分类和项目

--- app/src/main/java/com/example/homework/childFragment/Navigation.java
+++ app/src/main/java/com/example/homework/childFragment/Navigation.java
@@ -1,13 +1,25 @@
 package com.example.homework.childFragment;
 
+import static com.example.homework.Util.NetWorkGet.doGet;
+
+import android.graphics.Rect;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.homework.Adapter.NavLeftAdapter;
+import com.example.homework.Adapter.NavRightAdapter;
 import com.example.homework.R;
+import com.example.homework.bean.NavBean;
+import com.google.gson.Gson;
 
 public class Navigation extends Fragment {
 
@@ -16,7 +28,10 @@ public class Navigation extends Fragment {
 
     private String mParam1;
     private String mParam2;
-
+    private RecyclerView mRecyclerViewLeft;
+    private NavLeftAdapter mNavLeftAdapter;
+    private RecyclerView mRecyclerViewRight;
+    private NavRightAdapter mNavRightAdapter;
     public Navigation() {
 
     }
@@ -43,5 +58,47 @@ public class Navigation extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_navigation, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        mRecyclerViewRight = view.findViewById(R.id.rv_right);
+        mRecyclerViewLeft = view.findViewById(R.id.rv_left);
+        mNavRightAdapter = new NavRightAdapter();
+        mNavLeftAdapter = new NavLeftAdapter(mRecyclerViewRight,mNavRightAdapter);
+        netLoadNav();
+        mRecyclerViewLeft.setAdapter(mNavLeftAdapter);
+        mRecyclerViewLeft.setLayoutManager(new LinearLayoutManager(getContext()));
+        mRecyclerViewLeft.addItemDecoration(new RecyclerView.ItemDecoration() {
+            @Override
+            public void getItemOffsets(@NonNull Rect outRect, @NonNull View view, @NonNull RecyclerView parent, @NonNull RecyclerView.State state) {
+                super.getItemOffsets(outRect, view, parent, state);
+                outRect.set(0,10,0,10);
+            }
+        });
+        mRecyclerViewLeft.addItemDecoration(new DividerItemDecoration(getContext(),DividerItemDecoration.VERTICAL));
+    }
+
+    private void netLoadNav() {
+        String url = "https://www.wanandroid.com/navi/json";
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                String result=doGet(url);
+                Gson gson=new Gson();
+                NavBean navBean=gson.fromJson(result,NavBean.class);
+                updateUINav(navBean);
+            }
+        }).start();
+    }
+
+    private void updateUINav(NavBean navBean) {
+        getActivity().runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                mNavLeftAdapter.setDate(navBean);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/homework/childFragment/item.java
+++ app/src/main/java/com/example/homework/childFragment/item.java
@@ -1,21 +1,40 @@
 package com.example.homework.childFragment;
 
+import static com.example.homework.Util.NetWorkGet.doGet;
+
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.homework.Adapter.itemAdapter;
 import com.example.homework.R;
+import com.example.homework.bean.itemBean;
+import com.example.homework.bean.itemChildBean;
+import com.google.gson.Gson;
 
-public class item extends Fragment {
+import java.util.ArrayList;
+import java.util.List;
 
+public class item extends Fragment implements AdapterView.OnItemSelectedListener {
+    private List<itemBean.DataDTO> mList = new ArrayList<>();
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
-
+    private List<String> mStringList = new ArrayList<>();
     private String mParam1;
     private String mParam2;
+    private Spinner mSpinner;
+    private RecyclerView mMRecyclerView;
+    private itemAdapter mItemAdapter;
 
     public item() {
     }
@@ -42,5 +61,87 @@ public class item extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_item, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        mStringList.add("请选择项目类别");
+        netLoadItem();
+        mSpinner = view.findViewById(R.id.sp_dropdown);
+        ArrayAdapter<String> startAdapter = new ArrayAdapter<String>(getContext(), R.layout.item_select, mStringList);
+        mSpinner.setAdapter(startAdapter);
+        mSpinner.setSelection(0);
+        mSpinner.setOnItemSelectedListener(this);
+        mMRecyclerView = view.findViewById(R.id.rv_item);
+        mMRecyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+    }
+
+    private void netLoadItem() {
+        String url = "https://www.wanandroid.com/project/tree/json";
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                String result = doGet(url);
+                Gson gson = new Gson();
+                itemBean itemBean = gson.fromJson(result, com.example.homework.bean.itemBean.class);
+                updateUIItem(itemBean);
+            }
+        }).start();
+    }
+
+    private void updateUIItem(itemBean itemBean) {
+        getActivity().runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                mList.addAll(itemBean.getData());
+                for (int i = 0; i < mList.size(); i++) {
+                    mStringList.add(mList.get(i).getName());
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+        mItemAdapter = new itemAdapter(getContext());
+        if (i == 0) {
+            return;
+        } else {
+            int id = mList.get(i - 1).getId();
+            netLoadItemChild(id);
+            mMRecyclerView.setAdapter(mItemAdapter);
+        }
+    }
+
+    private void netLoadItemChild(int id) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 40; i++) {
+                    String url = "https://www.wanandroid.com/project/list/" + i + "/json?cid=" + id;
+                    String result = doGet(url);
+                    Gson gson = new Gson();
+                    itemChildBean itemChildBean = gson.fromJson(result, com.example.homework.bean.itemChildBean.class);
+                    upDataUIChildItem(itemChildBean);
+                }
+            }
+        }).start();
+
+    }
+
+    private void upDataUIChildItem(itemChildBean itemChildBean) {
+        getActivity().runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                mItemAdapter.setData(itemChildBean);
+            }
+        });
+    }
+
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
     }
 }
